47059c1a4b00f1628d8d8739c1f6de028740747e
bijan ebrahimy <myopensourceprojects@gmail.com>
2014-05-21 22:02:20 +0430

    moved reports to directories

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-  /* navbar affix */
-  /*
-  $('.navbar').affix({
-    offset: {
-      top: function(){
-        return (this.top = $('.navbar').position().top);
-      }
-    }
-  });
-  * */
-  
-  /* Ajax Pagination*/
-  $(document).on('click', '.view_more', function(){
-    var parent = $(this).parent()
-    var next_page = $(this).attr('data-next_page')
-    if(next_page){
-      $(this).button('loading')
-      $.get("/reports/"+next_page+".html", function(data){
-        $(parent).replaceWith(data)
-      })
-    }
-  })
+
 })
